d0f70ef2b08b70d081095417e39582086e9f71c6
Kevin Tyrrell <kev070892@gmail.com>
2024-01-05 00:54:59 -0500

    doc: Update copyright

--- lua/WoWProfessionOptimizer/model/profession/recipe.lua
+++ lua/WoWProfessionOptimizer/model/profession/recipe.lua
@@ -1,5 +1,5 @@
 --[[
---    Copyright (C) 2023 Kevin Tyrrell
+--    Copyright (C) 2024 Kevin Tyrrell
 --
 --    This program is free software: you can redistribute it and/or modify
 --    it under the terms of the GNU General Public License as published by
